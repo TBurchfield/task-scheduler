03bff170278bf743efe7657fdc51077534d87588
8bit <tburchfi@nd.edu>
2018-04-22 01:02:38 -0400

    Add basic structure

---
+++ schedule.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+class Schedule:
+  def __init__():
+    pass
+
+  def add_task(task):
+    pass
+
+  def remove_task(name):
+    pass
+
+  def get_task_names():
+    pass
---
+++ task.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+class Task:
+  def __init__():
+    pass
+
+  def dependent_on(task):
+    pass
