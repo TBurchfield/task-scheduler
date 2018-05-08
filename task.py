ce73755b1582a81f70c1d7d9a9a53c9c1c48107f
8bit <tburchfi@nd.edu>
2018-05-07 20:20:39 -0400

    Initial

--- task.py
+++ task.py
@@ -1,10 +1,11 @@
 # Defines the Task class, should not be executed directly.
 class Task:
   def __init__(self):
-    pass
+    self.dependencies = []
+    self.due_date = None
 
-  def dependent_on(self, task):
-    pass
+  def get_dependencies(self, task):
+    return self.dependencies
 
- def get_due_date(self, task):
+  def get_due_date(self, task):
    return self.due_date
