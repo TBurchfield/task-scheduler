186024a289d5a1101078e581d048f39ba443d770
Gaju <ggatera@nd.edu>
2018-05-08 01:40:04 -0400

    parsing

--- scheduler.py
+++ scheduler.py
@@ -7,10 +7,10 @@ import task
 
 # Function
 def usage(exit_code=0):
-	print(''' Usage: scheduler.py add_task taskname, due_date
-	scheduler.py remove_tasktaskname
+	print(''' Usage: scheduler.py add_task taskname  due_date
+	scheduler.py remove_task taskname
 	scheduler.py get_task_list
-	scheduler.py add_dependency(d1, d2)
+	scheduler.py add_dependency d1 d2
 	
 	Note: taskname must be unique
 	''')
@@ -24,9 +24,21 @@ if __name__ == '__main__':
 	args = sys.argv[1:]
 	while args and len(args[0]) > 1:
 		arg = args.pop(0)
-		# take care of add_task, remove_task, get_task_list
 		if arg == '-h':
-			usage(0)
+			usage(0)		
+		if sys.argv[2] == 'add_task':
+			taskname = args.pop(1)
+			due_date = args.pop(0)
+			add_task(taskname)
+		elif sys.argv[2] == 'remove_task':
+			taskname = args.pop(0)
+			remove_task(taskname)
+		elif sys.argv[2] == 'get_task_list':
+			get_task_list()
+		elif sys.argv[2] == 'add_dependency':
+			d1 = args.pop(1)
+			d2 = args.pop(2)
+			add_dependency(d1, d2)		# not sure about this one bc in task.py add_dependcy take 1arg
 		else:
 			usage(1)
 		
